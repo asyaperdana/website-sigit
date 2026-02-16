32f5faf877345df43b9e184b60f2531938b78c96
asya perdana <asyaperdanaa@gmail.com>
2026-02-16 18:46:54 +0700

    fix(deployment): fix broken layout by wrapping CSS imports in script tag and setting NODE_ENV during build

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,22 @@
-import '../app.css'; import '$lib/styles/variables.css'; import '$lib/styles/base.css'; import
-'$lib/styles/components.css'; import '$lib/styles/animations.css'; import '$lib/styles/theme.css';
+<script>
+	import '../app.css';
+	import '$lib/styles/variables.css';
+	import '$lib/styles/base.css';
+	import '$lib/styles/components.css';
+	import '$lib/styles/animations.css';
+	import '$lib/styles/theme.css';
+</script>
 
 <svelte:head>
 	<link
 		rel="stylesheet"
 		href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+	/>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@400;500;600;700;800&display=swap"
+		rel="stylesheet"
 	/>
 </svelte:head>
 
